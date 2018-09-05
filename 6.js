24bf2f366dff7138395a174dac878ab08b0281c9
Dmitry <31066789+altemuller@users.noreply.github.com>
2018-09-05 14:53:51 +0300

    Create 6.js

---
+++ 6.js
@@ -0,0 +1,19 @@
+function countBs(string) {
+  var count = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == "B")
+	  count++
+  }
+  return count;
+}
+
+function countChar(string, char) {
+  var count = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == char)
+	  count++
+  }
+  return count;
+}
+
+console.log(countBs('BABABA'), countChar('AHAHHA', 'H'));
